f7505b700e4a9e1dbbbb7358a7e72b6767d5541a
Rizki Adrian <rizkiadrian39@gmail.com>
2018-08-01 11:39:55 +0700

    refactor main consumer page

--- App/Components/Templates/MainConsumer/index.js
+++ App/Components/Templates/MainConsumer/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import ScrollableTabView from "react-native-scrollable-tab-view";
+import ScrollableTabView from 'react-native-scrollable-tab-view';
 import {
 	DashboardComponent,
 	NotificationComponent,
