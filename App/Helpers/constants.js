5ae4e0b1167587ea4376cffc811d8ee083565a10
Rizki Adrian <rizkiadrian39@gmail.com>
2018-09-07 12:55:46 +0700

    change api to production

--- App/Helpers/constants.js
+++ App/Helpers/constants.js
@@ -1,6 +1,6 @@
 //  API endpoint target
 export default {
-	api_url: 'https://ayo-api.dxtr.asia/',
+	api_url: 'https://api-ws.src.id/',
 	oauth_identifier: {
 		client_id: 2,
 		client_secret: '0QkMRDzTDe4cpLOYUHdDsqG70msWBX5q1zWXpH0D',
