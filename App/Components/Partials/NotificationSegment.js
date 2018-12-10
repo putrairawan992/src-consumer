c0c1f57db490e9cceebb0788acd03ef82e830d7b
Rizki Adrian <rizkiadrian39@gmail.com>
2018-12-10 12:47:20 +0700

    Add notification date

--- App/Components/Partials/NotificationSegment.js
+++ App/Components/Partials/NotificationSegment.js
@@ -5,9 +5,13 @@ import * as actions from '@templates/Notification/actions';
 import { CommonService } from '@services';
 import { Actions } from 'react-native-router-flux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import moment from 'moment';
 
 
 const styles = {
+	mainContainer: {
+		flexDirection: 'column',
+	},
 	segmentContainer: {
 		flexDirection: 'row',
 		alignItems: 'center',
@@ -36,6 +40,13 @@ const styles = {
 		height: 12,
 		width: 12,
 		borderRadius: 20
+	},
+	notifDate: {
+		fontFamily: 'ProximaNova-Regular',
+		fontSize: 12,
+		color: '#000',
+		paddingHorizontal: 20,
+		paddingTop: 20
 	}
 };
 
@@ -73,31 +84,36 @@ class NotificationSegment extends Component {
 	render() {
 		return (
 			<TouchableWithoutFeedback onPress={this.redirectNotification.bind(this)}>
-				<View style={styles.segmentContainer}>
-					<View
-						style={{
-							flex: 1,
-							flexDirection: 'row',
-							justifyContent: 'space-between',
-							alignItems: 'center'
-						}}
-					>
-						<View style={{ position: 'relative', flex: 1 }}>
-							<Image
-								source={require('@images/message-notif.png')}
-								style={{ height: 35, width: 35 }}
-							/>
-							{this.renderBadge()}
+				<View style={styles.mainContainer}>
+					<Text style={styles.notifDate}>
+						{moment(this.props.item.updated_at).format('DD MMM YYYY HH:mm')}
+					</Text>
+					<View style={styles.segmentContainer}>
+						<View
+							style={{
+								flex: 1,
+								flexDirection: 'row',
+								justifyContent: 'space-between',
+								alignItems: 'center'
+							}}
+						>
+							<View style={{ position: 'relative', flex: 1 }}>
+								<Image
+									source={require('@images/message-notif.png')}
+									style={{ height: 35, width: 35 }}
+								/>
+								{this.renderBadge()}
+							</View>
+							<View style={[styles.messageDescription, { flex: 3 }]}>
+								<Text style={styles.notifTitle}>
+									{this.props.item.title}
+								</Text>
+								<Text style={[styles.notifContent, (this.props.item.status === 'unread') ? { fontFamily: 'ProximaNova-Bold' } : null]}>
+									{this.props.item.content}
+								</Text>
+							</View>
+							<Icon name="keyboard-arrow-right" size={36} color="#000" />
 						</View>
-						<View style={[styles.messageDescription, { flex: 3 }]}>
-							<Text style={styles.notifTitle}>
-								{this.props.item.title}
-							</Text>
-							<Text style={[styles.notifContent, (this.props.item.status === 'unread') ? { fontFamily: 'ProximaNova-Bold' } : null]}>
-								{this.props.item.content}
-							</Text>
-						</View>
-						<Icon name="keyboard-arrow-right" size={36} color="#000" />
 					</View>
 				</View>
 			</TouchableWithoutFeedback>
